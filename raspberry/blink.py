9723e837c84e86cf99e7d753f89aceca44dd06c4
clyman88 <clyman@charlottesvilleschools.org>
2021-11-16 15:25:08 +0000

    Added raspberry folder

---
+++ raspberry/blink.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+channel_list = [5, 19, 26]
+
+GPIO.setup(channel_list, GPIO.OUT, initial=GPIO.LOW)
+
+def solo(lis, a, b):
+	for i in lis:
+		if i !=a:
+			GPIO.output(i, False)
+		else:
+			GPIO.output(i, True)
+	sleep(b)
+
+while True:
+	for i in channel_list:
+		solo(channel_list, i, .25)
