1863ee569067ad52b68e42ba41697076cf68b75b
clyman88 <clyman@charlottesvilleschools.org>
2021-12-16 14:37:42 +0000

    Added stop motion folder and files

---
+++ Pics/stop_motion.py
@@ -0,0 +1,25 @@
+import time
+import picamera
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+toggle = False
+
+num = 1
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    time.sleep(2)
+    print("Ready!")
+    while True:
+        if GPIO.input(4) == False and not toggle:
+            toggle = True
+            camera.capture("Stop-Motion/pic" + str(num) + ".jpg")
+            print("Picture taken ("+str(num)+").")
+            num += 1
+        elif GPIO.input(4) == True:
+            toggle = False
+
