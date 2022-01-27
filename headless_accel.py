af2acb2d675b1108148dc19b8dbfd961b7ffa88e
clyman88 <54861536+clyman88@users.noreply.github.com>
2022-01-27 10:33:33 -0500

    Update headless_accel.py

--- headless_accel.py
+++ headless_accel.py
@@ -1,3 +1,10 @@
+# Headless Accelerometer
+
+# Written by Cole Lyman
+
+# 11.30.2021
+
+#import important libraries
 import time
 
 import random
@@ -13,6 +20,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+#defining pins and accelerometer/LCD
 lsm303 = Adafruit_LSM303.LSM303()
 
 RST = 24
@@ -23,11 +31,13 @@ SPI_DEVICE = 0
 
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
 
+#init for display
 disp.begin()
 
 disp.clear()
 disp.display()
 
+#setting parameters for the image
 width = disp.width
 height = disp.height
 image = Image.new('1', (width,height))
@@ -37,6 +47,8 @@ draw = ImageDraw.Draw(image)
 font = ImageFont.load_default()
 x = 80
 y = 25
+
+#failed experiment :(
 collision = False
 
 randx = randint(0, width-10)
@@ -45,7 +57,8 @@ randy = randint(0, height-10)
 points = 0
 
 while True:
-
+  
+  #all of this is very similar to the I2C assignment
   disp.clear()
 
   draw.rectangle((0,0,width,height), outline=0, fill=0)
@@ -56,6 +69,7 @@ while True:
   draw.text((2, 10), "Y: " + str(round(accel_y*.01,2)), font=font, fill=255)
   #draw.text((2, 54), "Points: " +str(points), font=font, fill=255)
 
+  #more failed collision tests
   if x+15 >= randx & x+15 <= randx+10 & y+15 >= randy & y+15 <= randy+10:
     collision = True
 
@@ -67,7 +81,8 @@ while True:
   #draw.ellipse((randx,randy,randx+10,randy+10),outline=255, fill=0)
 
   y += round(accel_x*.01)
-
+  
+  #make sure circle doesn't go off the screen
   if y > height-15:
     y = height-15
   if y < 0:
@@ -79,7 +94,8 @@ while True:
     x = width - 15
   if x < 0:
     x = 0
-
+    
+  #actual drawing of circle
   draw.ellipse((x, y, x+15, y+15), outline=255, fill=0)
 
   disp.image(image)
